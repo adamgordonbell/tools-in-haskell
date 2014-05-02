df74a3912ac28f5fd655548ca7ae8ac6fe3c7f55
Agbell <agbell@gmail.com>
2014-05-02 15:39:42 +0000

    count words

--- src/Main.hs
+++ src/Main.hs
@@ -5,14 +5,14 @@ module Main where
 --import Text.ParserCombinators.Parsec
 --import Control.Monad.State
 --import Control.Exception
-import System.IO
-import Control.Monad
+import           Control.Monad
+import           System.IO
 
 -- | The main entry point.
 main :: IO ()
 main = do
     contents <- getTildeContents
-    print $ countLines  contents
+    print $ countWords contents
 
 getTildeContents :: IO String
 getTildeContents = liftM (takeTill '~') getContents
@@ -30,6 +30,25 @@ count = length
 
 countLines :: String -> Int
 countLines input = foldr count1 0 input
-    where 
+    where
         count1 '\n' n = n + 1
         count1 _ n = n
+
+countWords :: String -> Int
+countWords input = length $ splitMany "\n \t\r" input
+
+splitMany :: Eq a => [a] -> [a] -> [[a]]
+splitMany [] list = [list]
+splitMany (x:xs) list = clearEmpty . join $ (liftM $ splitMany xs) step
+  where step = split x list
+
+split :: Eq a => a -> [a] -> [[a]]
+split c s = case rest of
+                []     -> [chunk]
+                _:rest -> chunk : split c rest
+  where (chunk, rest) = break (==c) s
+
+clearEmpty :: [[a]] -> [[a]]
+clearEmpty [] = []
+clearEmpty ([]:xs) = clearEmpty xs
+clearEmpty (x:xs) = x:(clearEmpty xs)
